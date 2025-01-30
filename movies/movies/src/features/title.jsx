6834718894f4a8846ad98f273f90cd41ceffbb25
rushikeshballa <rushikeshballa27@gmail.com>
2025-01-30 11:53:32 +0530

    asd

--- movies/movies/src/features/title.jsx
+++ movies/movies/src/features/title.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useGetMoviesbytitleQuery, useGetMoviesQuery } from "../services/moviesApi";
 
 function Titles(){
+  
     var {isloding,data}=useGetMoviesQuery() 
   return (
     <div className='p-3'>
