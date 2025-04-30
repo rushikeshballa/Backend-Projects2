171c3fca94fa9b330f05f43a77a3775bb6a219ee
rushikeshballa <rushikeshballa27@gmail.com>
2025-04-30 11:47:13 +0530

    asd

--- movies/movies/src/features/movie.jsx
+++ movies/movies/src/features/movie.jsx
@@ -26,6 +26,7 @@ const Movie = (() => {
                                 })
                             }
                         </div>
+                        
                         <br />
                         {<div>
                             <h4>About the movie :-</h4>
